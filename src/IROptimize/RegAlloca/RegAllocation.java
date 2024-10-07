5800ccc06ffa637f3395c4da04f87f09dbabe76a
userhhhhh <hqs123@sjtu.edu.cn>
2024-10-07 20:07:52 +0800

    pass DCE and LiveAnalysis no compile & binary error

--- src/IROptimize/RegAlloca/RegAllocation.java
+++ src/IROptimize/RegAlloca/RegAllocation.java
@@ -12,6 +12,6 @@ public class RegAllocation {
 
     public void work() {
         new DCE(program).work();
-//        new LiveAnalysis(program).work();
+        new LiveAnalysis(program).work();
     }
 }
